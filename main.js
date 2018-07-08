148c30fc243ef485ff03731348f255d40f2d8aea
Daniel Ziga <dazig90@gmail.com>
2018-07-08 14:21:13 +0200

    Initial setup

---
+++ main.js
@@ -0,0 +1,26 @@
+module.exports.loop = () => {
+  clearMemory();
+};
+
+function clearMemory() {
+  for (const room in Memory.rooms) {
+    if (!Game.rooms[room]) {
+      delete Memory.rooms[room];
+    }
+  }
+  for (const spawn in Memory.spawns) {
+    if (!Game.spawns[spawn]) {
+      delete Memory.spawns[spawn];
+    }
+  }
+  for (const creep in Memory.creeps) {
+    if (!Game.creeps[creep]) {
+      delete Memory.creeps[creep];
+    }
+  }
+  for (const flag in Memory.flags) {
+    if (!Game.flags[flag]) {
+      delete Memory.flags[flag];
+    }
+  }
+}
