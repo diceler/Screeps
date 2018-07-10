d7aa23eebc419f6e294813b9dc4f0f7a0efe6156
Daniel Ziga <dazig90@gmail.com>
2018-07-10 20:05:20 +0200

    Fix calling of bodyTypes

--- spawn.extend.js
+++ spawn.extend.js
@@ -58,10 +58,10 @@ Spawn.prototype.spawnCreep = function (role, memory) {
 
 Spawn.prototype.getCreepBody = function (role) {
   let rcl = this.room.controller.level;
-  let body = this.bodyTypes(role, rcl);
+  let body = this.bodyTypes[role](rcl);
 
   while (rcl > 0 && !this.canCreateCreep(body)) {
-    body = rcl > 0 ? this.bodyTypes(role, --rcl) : undefined;
+    body = rcl > 0 ? this.bodyTypes[role](--rcl) : undefined;
   }
 
   return body;
@@ -86,50 +86,50 @@ Spawn.prototype.bodyTypes = {
   [ROLE.UPGRADER]: rcl => {
     switch (rcl) {
       case 1:
-        return this.workerUnitBuild(2, 1, 1);
+        return workerUnitBuild(2, 1, 1);
       case 2:
-        return this.workerUnitBuild(2, 4, 2);
+        return workerUnitBuild(2, 4, 2);
       case 3:
-        return this.workerUnitBuild(4, 6, 2);
+        return workerUnitBuild(4, 6, 2);
       case 4:
-        return this.workerUnitBuild(5, 10, 5);
+        return workerUnitBuild(5, 10, 5);
       default:
-        return this.workerUnitBuild(10, 10, 5);
+        return workerUnitBuild(10, 10, 5);
     }
   },
   [ROLE.BUILDER]: rcl => {
     switch (rcl) {
       case 1:
-        return this.workerUnitBuild(1, 2, 1);
+        return workerUnitBuild(1, 2, 1);
       case 2:
-        return this.workerUnitBuild(2, 4, 2);
+        return workerUnitBuild(2, 4, 2);
       case 3:
-        return this.workerUnitBuild(4, 6, 2);
+        return workerUnitBuild(4, 6, 2);
       default:
-        return this.workerUnitBuild(5, 10, 5);
+        return workerUnitBuild(5, 10, 5);
     }
   },
   [ROLE.HARVESTER]: rcl => {
     switch (rcl) {
       case 1:
       case 2:
-        return this.workerUnitBuild(1, 1, 1);
+        return workerUnitBuild(1, 1, 1);
       default:
-        return this.workerUnitBuild(5, 1, 5);
+        return workerUnitBuild(5, 1, 5);
     }
   },
   [ROLE.HAULER]: rcl => {
     switch (rcl) {
       case 1:
       case 2:
-        return this.workerUnitBuild(0, 4, 2);
+        return workerUnitBuild(0, 4, 2);
       default:
-        return this.workerUnitBuild(0, 8, 8);
+        return workerUnitBuild(0, 8, 8);
     }
   },
 };
 
-Spawn.prototype.workerUnitBuild = function (workParts, carryParts, moveParts) {
+function workerUnitBuild(workParts, carryParts, moveParts) {
   function addBodyParts(part, amount) {
     let partialBody = [];
     let increment = 0;
@@ -147,4 +147,4 @@ Spawn.prototype.workerUnitBuild = function (workParts, carryParts, moveParts) {
   }
 
   return addBodyParts(WORK, workParts).concat(addBodyParts(CARRY, carryParts)).concat(addBodyParts(MOVE, moveParts));
-};
+}
