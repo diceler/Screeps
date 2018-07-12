43f6c366cf673a1d47bef9dab855d8127a2e7ac2
Daniel Ziga <dazig90@gmail.com>
2018-07-12 20:13:59 +0200

    Deleted moveTo override

--- creep.extend.js
+++ creep.extend.js
@@ -26,19 +26,3 @@ Creep.prototype.loop = function () {
       break;
   }
 };
-
-Creep.prototype.__moveTo = Creep.prototype.moveTo;
-Creep.prototype.moveTo = function (target, options) {
-  if (this.isFatigued) {
-    return ERR_TIRED;
-  }
-
-  const pathLength = _.size(_.get(this.memory._move, 'path', null));
-
-  const opts = _.assign({
-    reusePath: this.isStuck ? 0 : pathLength,
-    noPathFinding: !this.isStuck && pathLength > 0,
-  }, options);
-
-  this.__moveTo(target, opts);
-};
