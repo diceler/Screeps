bd619d406eea85b4f5e91cdec2c51e4466416f40
Daniel Ziga <dazig90@gmail.com>
2018-11-30 23:00:29 +0100

    Create spawn request if source not occupied

--- room.extend.js
+++ room.extend.js
@@ -1,10 +1,16 @@
 'use strict';
 
 Room.prototype.loop = function () {
-  this.sources;
+  _.forEach(this.sources, sourceId => {
+    const source = getObjectById(sourceId);
+
+    if (!source.occupied) {
+      this.createSpawnRequest(ROLE_HARVESTER, {sourceId}, sourceId);
+    }
+  });
 };
 
-Room.prototype.createSpawnRequest = function (role, memory) {
+Room.prototype.createSpawnRequest = function (role, memory, requesterId) {
   if (!role || !_.isObject(memory)) {
     console.log('ERR', `Can't create spawn request for ${role} in ${this.name}`);
     return ERR_INVALID_ARGS;
@@ -12,10 +18,16 @@ Room.prototype.createSpawnRequest = function (role, memory) {
 
   this.memory.spawnQueue = this.memory.spawnQueue || [];
 
-  this.memory.spawnQueue.push({
-    role,
-    memory,
-    base: this.name,
-    priority: SPAWN_PRIORITY[role],
-  });
+  if (!_.find(this.memory.spawnQueue, {requesterId})) {
+    this.memory.spawnQueue.push({
+      requesterId,
+      role,
+      memory: _.assign({}, memory, {base: this.name}),
+      priority: SPAWN_PRIORITY[role],
+    });
+
+    return OK;
+  }
+
+  return ERR_NAME_EXISTS;
 };
