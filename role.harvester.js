6641bef710518251f0aacd108fb195a4fb149e3a
Daniel Ziga <dazig90@gmail.com>
2018-12-01 12:30:18 +0100

    Implement Harvester cycle. Add definitions to Structure

--- role.harvester.js
+++ role.harvester.js
@@ -15,12 +15,51 @@ class Harvester extends CreepBase {
     }
   }
 
+  findStorage() {
+    const storages = this.creep.room.find(FIND_MY_STRUCTURES, {filter: structure => structure.storesEnergy && !structure.isFull});
+    const storage = _.first(storages);
+
+    if (storage) {
+      this.creep.memory.storageId = storage.id;
+    } else {
+      console.log('ERR: All storages are full');
+    }
+
+    return storage;
+  }
+
   tick() {
     const creep = this.creep;
-    let source = Game.getObjectById(creep.memory.sourceId);
 
-    if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(source);
+    if (!creep.isFull) {
+      let source = Game.getObjectById(creep.memory.sourceId);
+
+      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
+      }
+    } else {
+      let storage;
+
+      if (!creep.memory.storageId) {
+        storage = this.findStorage();
+
+        if (!storage) {
+          return;
+        }
+      } else {
+        storage = getObjectById(creep.memory.storageId);
+      }
+
+      let actionResult = creep.transfer(storage, RESOURCE_ENERGY);
+
+      switch (actionResult) {
+        case ERR_NOT_IN_RANGE:
+          creep.moveTo(storage);
+          break;
+        case ERR_FULL:
+          delete creep.memory.storageId;
+          break;
+      }
     }
   }
 }
---
+++ structure.define.js
@@ -0,0 +1,55 @@
+Object.defineProperty(Structure.prototype, 'memory', {
+  configurable: true,
+  get: function () {
+    if (!Memory.structures) {
+      Memory.structures = {};
+    }
+
+    if (!_.isObject(Memory.structures)) {
+      return undefined;
+    }
+
+    return Memory.structures[this.id] = Memory.structures[this.id] || {};
+  },
+  set: function (value) {
+    if (!Memory.structures) {
+      Memory.structures = {};
+    }
+
+    if (!_.isObject(Memory.structures)) {
+      throw new Error('Could not set structure memory');
+    }
+
+    Memory.structures[this.id] = value;
+  }
+});
+
+Object.defineProperty(Structure.prototype, 'storesEnergy', {
+  configurable: true,
+  get: function () {
+    if (!this._storesEnergy) {
+      if (!this.store) {
+        this._storesEnergy = !!this.energy;
+      } else {
+        this._storesEnergy = (RESOURCE_ENERGY in this.store);
+      }
+    }
+
+    return this._storesEnergy;
+  }
+});
+
+Object.defineProperty(Structure.prototype, 'isFull', {
+  configurable: true,
+  get: function () {
+    if (!this._isFull) {
+      if (!this.store) {
+        this._isFull = this.energy === this.energyCapacity;
+      } else {
+        this._isFull = _.sum(this.store) === this.storeCapacity;
+      }
+    }
+
+    return this._isFull;
+  }
+});
