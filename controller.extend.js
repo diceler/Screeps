091a37d4b124ad797c470ce8a3e208a95a674ad0
Daniel Ziga <dazig90@gmail.com>
2018-12-03 23:12:38 +0100

    Fix upgrader spawn condition

--- controller.extend.js
+++ controller.extend.js
@@ -1,8 +1,8 @@
 'use strict';
 
 StructureController.prototype.tick = function () {
-  const hasDowngradeMinBeenReached_and_hasThereBeenNoInteraction = (this.ticksToDowngradeMinReached && this.ticksSinceUpgrade > NO_INTERACTION_TICK_LIMIT);
-  const hasThereBeenNoInteraction_and_insufficientUpgraders = (this.ticksSinceUpgrade > NO_INTERACTION_TICK_LIMIT && !this.sufficientUpgraders);
+  const hasDowngradeMinBeenReached_and_hasThereBeenNoInteraction = (this.ticksToDowngradeMinReached && this.ticksSinceUpgrade >= NO_INTERACTION_TICK_LIMIT);
+  const hasThereBeenNoInteraction_and_insufficientUpgraders = (this.ticksSinceUpgrade >= NO_INTERACTION_TICK_LIMIT && !this.sufficientUpgraders);
 
   if (hasDowngradeMinBeenReached_and_hasThereBeenNoInteraction || hasThereBeenNoInteraction_and_insufficientUpgraders) {
     this.room.createSpawnRequest(this.id, ROLE_UPGRADER);
