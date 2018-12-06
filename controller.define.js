f6998dc3589d0c58fcd0d352981205fdb69084bd
Daniel Ziga <dazig90@gmail.com>
2018-12-06 21:15:27 +0100

    Refactor upgrader spawn logic

--- controller.define.js
+++ controller.define.js
@@ -12,19 +12,16 @@ Object.defineProperty(StructureController.prototype, 'hasSufficientUpgraders', {
   get: function () {
     const upgraders = _.size(this.room.creeps[ROLE_UPGRADER]);
 
-    switch (this.level) {
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 8:
-        return upgraders >= 1;
-      case 5:
-      case 6:
-      case 7:
-        return upgraders >= 2;
-      default:
-        return true;
+    if (upgraders === 0) {
+      return false;
     }
+
+    const harvestersInRoom = _.size(this.room.creeps[ROLE_HARVESTER]);
+
+    if (harvestersInRoom > 1) {
+      return Math.floor(harvestersInRoom / 2) <= upgraders;
+    }
+
+    return true;
   }
 });
